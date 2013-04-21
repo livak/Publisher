26d4883eaea85781d076f0393ee89d1c0ed4622f
livak <livakgit@gmail.com>
2013-04-21 02:13:38 +0200

    first change

--- WpfClient/MainWindow.xaml.cs
+++ WpfClient/MainWindow.xaml.cs
@@ -1,46 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-using System.ComponentModel;
-using System.Collections.ObjectModel;
 
 namespace WpfClient
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
-
-        PowerMonitoringViewModel pwmViewModel = new PowerMonitoringViewModel();
+        private readonly PowerMonitoringViewModel _pwmViewModel = new PowerMonitoringViewModel();
 
         public MainWindow()
         {
             InitializeComponent();
-           this.DataContext = pwmViewModel;
+            DataContext = _pwmViewModel;
 
 
         }
 
         private void btnGetLastDay_Click(object sender, RoutedEventArgs e)
         {
-            NetVariableViewModel variable = lbxVariable.SelectedItem as NetVariableViewModel;
-            if (variable!=null)
+            var variable = lbxVariable.SelectedItem as NetVariableViewModel;
+            if (variable != null)
             {
-                pwmViewModel.GetAverageLastDay(variable.Name);
+                _pwmViewModel.GetAverageLastDay(variable.Name);
             }
 
         }
-    } 
+    }
 }
