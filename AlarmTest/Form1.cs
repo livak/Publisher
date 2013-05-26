83fd1c53958b55c56b2f68a64d5088d2f6ca2739
livak <livakgit@gmail.com>
2013-05-26 16:05:17 +0200

    Move AlarmTest to .net 4.5 because of National Instruments Measurement Studio

--- AlarmTest/Form1.cs
+++ AlarmTest/Form1.cs
@@ -1,25 +1,4 @@
-﻿using NationalInstruments.Analysis;
-using NationalInstruments.Analysis.Conversion;
-using NationalInstruments.Analysis.Dsp;
-using NationalInstruments.Analysis.Dsp.Filters;
-using NationalInstruments.Analysis.Math;
-using NationalInstruments.Analysis.Monitoring;
-using NationalInstruments.Analysis.SignalGeneration;
-using NationalInstruments.Analysis.SpectralMeasurements;
-using NationalInstruments;
-using NationalInstruments.NetworkVariable;
-using NationalInstruments.NetworkVariable.WindowsForms;
-using NationalInstruments.Tdms;
-using NationalInstruments.UI;
-using NationalInstruments.UI.WindowsForms;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace AlarmTest
 {
